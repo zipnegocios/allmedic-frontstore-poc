a59f1537ce2335bdd3d2025e43687f6c9bcf175a
Gustavo Amarista <zipnegocios@gmail.com>
2026-07-10 04:43:48 -0400

    FIX: agregar footer al corporativo

--- src/app/(store)/corporativo/layout.tsx
+++ src/app/(store)/corporativo/layout.tsx
@@ -1,6 +1,7 @@
 import { CorporateCartProvider } from '@/context/CorporateCartContext';
 import { CorporateCartButton } from '@/components/corporate/CorporateCartButton';
 import { CorporateAccountLink } from '@/components/corporate/CorporateAccountLink';
+import { Footer } from '@/components/layout/Footer';
 
 export default function CorporativoLayout({ children }: { children: React.ReactNode }) {
   return (
@@ -8,6 +9,7 @@ export default function CorporativoLayout({ children }: { children: React.ReactN
       {children}
       <CorporateAccountLink />
       <CorporateCartButton />
+      <Footer />
     </CorporateCartProvider>
   );
 }
